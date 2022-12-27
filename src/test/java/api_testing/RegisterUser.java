72c9b653f87b0071759a9b376c70ea907bbbf35a
NourhanFekry <nurhaaaaan9@gmail.com>
2022-12-27 12:30:30 +0200

    Edit test class

--- src/test/java/api_testing/RegisterUser.java
+++ src/test/java/api_testing/RegisterUser.java
@@ -6,6 +6,7 @@ import com.shaft.driver.SHAFT;
 import io.restassured.http.ContentType;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import pages.LoginPage;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,65 +15,55 @@ public class RegisterUser
 {
 SHAFT.API api;
 String url = System.getProperty("automationExerciseBaseUrl");
-private String email;
-private String password;
-private String name;
+SHAFT.GUI.WebDriver driver;
+LoginPage loginPage;
+private String email = "nawara@Test.com";
+private String password = "246810";
+private String name = "Nourhan";
 
     @BeforeClass
     public void setup()
     {
     api = new SHAFT.API(url);
-    }
-   //////////API 11: POST To Create/Register User Account\\\\\\\\\\\\
-    @Test
-    public void createRegisterUser()
-    {
+    driver = new SHAFT.GUI.WebDriver();
+    driver.browser().navigateToURL("https://automationexercise.com/login");
         List <List<Object>> registerData= Arrays.asList
-        (
-                Arrays.asList("name","Nourhan"),
-                Arrays.asList("email","nawara@Test.com"),
-                Arrays.asList("password","246810"),
-                Arrays.asList("title","Ms"),
-                Arrays.asList("brith_date","28"),
-                Arrays.asList("birth_month","02"),
-                Arrays.asList("birth_year","2000"),
-                Arrays.asList("firstname","Nourhan"),
-                Arrays.asList("lastname","Fekry"),
-                Arrays.asList("company","VOIS"),
-                Arrays.asList("address1","Mansoura"),
-                Arrays.asList("address2","Mansoura"),
-                Arrays.asList("country","India"),
-                Arrays.asList("zipcode","1234"),
-                Arrays.asList("state","Cairo"),
-                Arrays.asList("city","cairo"),
-                Arrays.asList("mobile_number","01234567890")
-        );
+                (
+                        Arrays.asList("name","Nourhan"),
+                        Arrays.asList("email","nawara@Test.com"),
+                        Arrays.asList("password","246810"),
+                        Arrays.asList("title","Ms"),
+                        Arrays.asList("brith_date","28"),
+                        Arrays.asList("birth_month","02"),
+                        Arrays.asList("birth_year","2000"),
+                        Arrays.asList("firstname","Nourhan"),
+                        Arrays.asList("lastname","Fekry"),
+                        Arrays.asList("company","VOIS"),
+                        Arrays.asList("address1","Mansoura"),
+                        Arrays.asList("address2","Mansoura"),
+                        Arrays.asList("country","India"),
+                        Arrays.asList("zipcode","1234"),
+                        Arrays.asList("state","Cairo"),
+                        Arrays.asList("city","cairo"),
+                        Arrays.asList("mobile_number","01234567890")
+                );
         api.post("/createAccount")
                 .setParameters(registerData, RestActions.ParametersType.FORM)
                 .setContentType(ContentType.URLENC)
                 .perform();
-
-        ///////////// try to get email and password from api body\\\\\\\\\\\\\\\
-       /*
-        List<Object> emailValue = registerData.get(1);
-        email  = emailValue.get(1).toString();
-        List<Object> passwordValue = registerData.get(2);
-        password  = passwordValue.get(1).toString();
-        */
     }
-    public String getEmail()
+    @Test
+    public void loginWithCorrectData()
     {
-        email = "nawaraTest@gmail.com";
-        return email;
-    }
-    public String getPassword()
-    {
-        password = "246810";
-        return  password;
-    }
-    public String getName()
-    {
-        name = "Nourhan";
-        return name;
+        loginPage = new LoginPage(driver);
+        System.out.println("this is for test email "+ email);
+        System.out.println("this is for test password "+password);
+        loginPage.enterDataInLoginEmailFiled(email,password);
+        loginPage.clickLoginButton();
+        SHAFT.Validations
+                .assertThat()
+                .object(loginPage.checkLoginSuccessfully()
+                .contains("Logged in as "+name));
+        System.out.println("pass");
     }
 }
